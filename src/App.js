b6f3828c316da3f977b0e40e0bc3d95394c36cbc
rabiulim <rabiulim2022@gmail.com>
2022-05-22 18:09:36 +0600

    navbar, footer , login,signup added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import SignUp from './components/Login/SignUp';
+import Footer from './components/Shared/Footer';
 import Navbar from './components/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
