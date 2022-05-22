93dafdcdaddd7d07eb646801d8237e525eb8a369
rabiulim <rabiulim2022@gmail.com>
2022-05-23 00:39:00 +0600

    loading compo add and make toolbuy private

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './components/Home/Home';
 import ToolBuy from './components/Home/ToolBuy';
 import Login from './components/Login/Login';
+import RequireAuth from './components/Login/RequireAuth';
 import SignUp from './components/Login/SignUp';
 import Footer from './components/Shared/Footer';
 import Navbar from './components/Shared/Navbar';
@@ -16,7 +17,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/signup' element={<SignUp />}></Route>
-        <Route path='/tool/:toolId' element={<ToolBuy />}></Route>
+        <Route path='/tool/:toolId' element={<RequireAuth><ToolBuy /></RequireAuth>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
