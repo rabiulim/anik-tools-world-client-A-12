2cc5c4202028b0df1d48bf0c347494949c8db262
rabiulim <rabiulim2022@gmail.com>
2022-05-26 00:31:02 +0600

    order page work done and  button disabled done

--- src/components/Dashboard/MyOrder.js
+++ src/components/Dashboard/MyOrder.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 const MyOrder = () => {
+
+    const [orders, setOrders] = useState([])
+
+    console.log(orders)
+
+    useEffect(() => {
+        const url = 'http://localhost:5000/order'
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setOrders(data))
+    }, [orders])
+
+    const handleOrderDelete = (id) => {
+        console.log(id)
+        const proceed = window.confirm('Are you sure you want to delete?');
+        if (proceed) {
+            console.log('deleting user with id, ', id);
+            const url = `http://localhost:5000/order/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log('deleted');
+                        const remaining = orders.filter(order => order._id !== id);
+                        setOrders(remaining);
+                    }
+                })
+        }
+    }
+
     return (
         <div>
-            <h1>this is my order page</h1>
+            <div class="overflow-x-auto w-full  w-full">
+                <table class="table table-zebra w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Order Quantity</th>
+                            <th>Payment</th>
+                            <th>Delet</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            orders.map(order => <tr key={order._id}>
+                                <th>1</th>
+                                <td>{order.userName}</td>
+                                <td>{order.product}</td>
+                                <td>{order.price}</td>
+                                <td>{order.orderQuantity}</td>
+                                <td><button className='btn btn-sm'>Pay Now</button></td>
+                                <td><button onClick={() => handleOrderDelete(order._id)} className='btn btn-sm'>Delete</button></td>
+
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/components/Home/ToolBuy.js
+++ src/components/Home/ToolBuy.js
@@ -10,37 +10,48 @@ const ToolBuy = () => {
 
     const [tool, setTool] = useState({})
 
-    console.log(user)
+    const [isDisabled, setDisabled] = useState(false);
+
+
+
 
     useEffect(() => {
         const url = `http://localhost:5000/tool/${toolId}`
         fetch(url)
             .then(res => res.json())
             .then(data => setTool(data))
-    }, [tool])
+    }, [])
 
     const handleBuyNow = event => {
         event.preventDefault();
         const minimumQuantity = tool.minimumOrder;
-        const inputQuantity = parseInt(event.target.quantity.value);
-        const updateQuantity = minimumQuantity + inputQuantity
+        const availableQuantity = tool.availableQuantity;
+        const orderQuantity = parseInt(event.target.quantity.value);
+        if (orderQuantity < minimumQuantity || orderQuantity > availableQuantity) {
+            setDisabled(true)
+            alert(`plz Minimum Quantity:${minimumQuantity}  available Quantity:${availableQuantity}`)
+            return
+        }
+        const updateQuantity = minimumQuantity + orderQuantity
+        const product = tool.name;
+        const price = tool.price;
         const userName = user.displayName;
         const userEmail = user.email;
         const phoneNumber = event.target.phoneNumber.value;
-        const userInfo = { userName, userEmail, phoneNumber, inputQuantity }
-        const url = 'http://localhost:5000/userinfo'
+        const orderInfo = { userName, userEmail, phoneNumber, orderQuantity, product, price }
+        const url = 'http://localhost:5000/order'
         fetch(url, {
             method: "POST",
             headers: {
                 "content-type": "application/json"
             },
-            body: JSON.stringify(userInfo)
+            body: JSON.stringify(orderInfo)
 
         })
             .then(res => res.json())
             .then(data => {
                 console.log('success', data)
-                alert('user  successfully added')
+                alert('orders  successfully added')
                 event.target.reset();
             })
     }
@@ -56,7 +67,8 @@ const ToolBuy = () => {
                 <form onSubmit={handleBuyNow} className='grid grid-cols-1 gap-3 justify-items-center mt-2'>
                     <input type="number" name='phoneNumber' placeholder="Plz Phone Number" class="input input-bordered input-primary w-full max-w-xs" />
                     <input type="number" name='quantity' placeholder="Plz Enter Quantity" class="input input-bordered input-primary w-full max-w-xs" />
-                    <input type="submit" value="Buy Now" className="btn btn-secondary w-full max-w-xs" />
+                    <input type="submit" value="Buy Now" className="btn btn-secondary w-full max-w-xs" disabled={isDisabled} />
+
                 </form>
             </div>
         </div>
