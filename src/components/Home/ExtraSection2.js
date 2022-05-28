e29caff23d4a5c73f6547447b2a9a3ce88224892
rabiulim <rabiulim2022@gmail.com>
2022-05-28 17:35:07 +0600

    home page finally done

---
+++ src/components/Home/ExtraSection2.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ExtraSection2 = () => {
+    return (
+        <div className='m-6'>
+            <div class="hero h-96" style={{ "backgroundImage": "URL('https://media.istockphoto.com/photos/auto-parts-picture-id478107962?b=1&k=20&m=478107962&s=170667a&w=0&h=6RVCyc6ojG63MPSWxfrLMebr5kObIahjBzk-4WZb7SE=')" }} >
+                <div class="hero-overlay bg-opacity-60"></div>
+                <div class="hero-content text-center text-neutral-content">
+                    <div class="max-w-md">
+                        <h1 class="mb-5 text-5xl font-bold">Do you want Latest!!</h1>
+                        <p class="mb-5 text-xl">If want to know our latest tools ,you have to suscribe own web.We will send you our latest tools info.If you interested click the subscribe button!! </p>
+                        <button class="btn btn-primary">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ExtraSection2;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import BusinessSummury from './BusinessSummury';
 import ExtraSection1 from './ExtraSection1';
+import ExtraSection2 from './ExtraSection2';
 import Reviews from './Reviews';
 import Tools from './Tools';
 
@@ -12,6 +13,7 @@ const Home = () => {
             <Tools></Tools>
             <BusinessSummury></BusinessSummury>
             <Reviews></Reviews>
+            <ExtraSection2></ExtraSection2>
             <ExtraSection1></ExtraSection1>
         </div>
     );
